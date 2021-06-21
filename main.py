12ec14affa24da4060c914243d9ec30190bb0976
warevil <60890246+warevil@users.noreply.github.com>
2021-06-21 16:36:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from learnabc import models
 from learnabc.database import engine
 from learnabc.routers import course, user, auth
@@ -6,6 +7,19 @@ from learnabc.routers import course, user, auth
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 app.include_router(course.router)
