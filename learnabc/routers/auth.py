bdaa569e030a344ee5586e1fa78dc2a3aff57a28
warevil <60890246+warevil@users.noreply.github.com>
2021-06-20 22:11:07 -0500

    Fixing imports
    Pending: Check unused imports

--- learnabc/routers/auth.py
+++ learnabc/routers/auth.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, status, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from .. import schemas, database, models, token
-from ..hashing import Hash
+from learnabc import schemas, database, models, token
+from learnabc.hashing import Hash
 from sqlalchemy.orm import Session
 router = APIRouter(tags=['Authentication'])
 
