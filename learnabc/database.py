985e7cc8f2062db8e49a607dd6267785b1b1b47a
rogrp <yihsic@gmail.com>
2021-06-30 13:51:33 -0500

    fix db uri

--- learnabc/database.py
+++ learnabc/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URI")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
