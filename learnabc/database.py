1991cc267dc979d2033d09611c2ff7a4240e39a8
rogrp <yihsic@gmail.com>
2021-06-30 13:43:36 -0500

    deploy configuration

--- learnabc/database.py
+++ learnabc/database.py
@@ -1,11 +1,10 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHAMY_DATABASE_URL = 'sqlite:///./learnabc.db'
-
-engine = create_engine(SQLALCHAMY_DATABASE_URL, connect_args={
-                       "check_same_thread": False})
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
 
